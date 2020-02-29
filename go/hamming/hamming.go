87f5506091bd1a47b27655bae781bceed00563fc
AJ Christensen <aj@junglistheavy.industries>
2020-02-29 13:49:59 +1300

    utf8 capabilities

--- go/hamming/hamming.go
+++ go/hamming/hamming.go
@@ -7,22 +7,18 @@ import (
 
 // Distance function calculates the Hamming distance between two strings of equal length: a,b
 // It will return either the distance, or error in case of a,b not having equal length.
-func Distance(a, b string) (int, error) {
-	if utf8.RuneCountInString(a) != utf8.RuneCountInString(b) {
-		return 0, errors.New("a,b must have same length to calculate Hamming distance")
+func Distance(aStr, bStr string) (int, error) {
+	if utf8.RuneCountInString(aStr) != utf8.RuneCountInString(bStr) {
+		return 0, errors.New("a,b must have same length (rune count in string) to calculate Hamming distance")
 	}
 
 	distance := 0
-	// for i, w := 0; i <= len(a); i += w {
-	// 	runeValue, width := utf8.DecodeRuneInString(a[i:])
-	// 	fmt.Printf("%#U starts at byte position %d\n", runeValue, i)
-	// 	w = width
-	// }
-
-	for i := range a {
-		if a[i] != b[i] {
+	for _, a := range aStr {
+		b, n := utf8.DecodeRuneInString(bStr)
+		if a != b {
 			distance++
 		}
+		bStr = bStr[n:]
 	}
 
 	return distance, nil
